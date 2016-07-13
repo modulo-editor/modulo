708f77d1f52ee1de30c5e66be527ab3ee462afec
Connor Brewster <connor.brewster@eagles.oc.edu>
2016-07-13 15:46:25 -0600

    Add travis add minor documentation

--- src/core/core_msg.rs
+++ src/core/core_msg.rs
@@ -1,7 +1,10 @@
 use ::file::file_msg::{FileId, ToFileThreadMsg};
 
+/// Messages that can be sent to the core thread.
 #[derive(Debug)]
 pub enum ToCoreThreadMsg {
+    /// Sends file thread message to the file thread matching the `FileId`
     FileThreadMsg(FileId, ToFileThreadMsg),
+    /// Spawns a new file thread
     SpawnFileThread,
 }
