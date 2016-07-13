71c8e95a2669850ed0efa95dd1d2677fa18e54a2
Connor Brewster <connor.brewster@eagles.oc.edu>
2016-07-13 15:59:28 -0600

    Add doc generation

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+cargo doc
+
+rev=$(git rev-parse --short HEAD)
+
+cd target/doc
+
+git init
+git config user.name "Connor Brewster"
+git config user.email "connor.brewster@eagles.oc.edu"
+
+git remote add upstream "https://$GH_TOKEN@github.com/modulo-editor/modulo.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
